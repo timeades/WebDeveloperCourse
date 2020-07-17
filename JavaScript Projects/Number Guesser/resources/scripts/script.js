1ec6c33e1309addea5e0a9b98217e10b257cb686
Tim Eades <tim.eades@mac.com>
2020-07-17 16:33:22 +0100

    script for the number guesser game

--- JavaScript Projects/Number Guesser/resources/scripts/script.js
+++ JavaScript Projects/Number Guesser/resources/scripts/script.js
@@ -1,3 +1,26 @@
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
+
+//Generate a number between 0 - 9
+const generateTarget = () => {
+    return (Math.floor(Math.random() * 10));
+};
+//Checking the guess to see who is closer to the target number. human wins in case of draw
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    const humanDifference = Math.abs(targetGuess - humanGuess);
+    const computerDifference = Math.abs(targetGuess - computerGuess);
+    return humanDifference <= computerDifference;
+};
+//assigning the winner of the game and awarding update to score for winner
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner === 'computer') {
+        computerScore++;
+    }
+};
+
+//Advancing the round number after each one
+const advanceRound = () => currentRoundNumber++;
+
